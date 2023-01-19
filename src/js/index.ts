503e4a6db1332aa56121568f10d61e59db8c87c8
LeafField <109466326+LeafField@users.noreply.github.com>
2023-01-19 20:13:20 +0900

    htmlマークアップ練習、その他テスト

---
+++ src/js/index.ts
@@ -0,0 +1,8 @@
+import "normalize.css";
+import "../styles/style.scss";
+import "@fortawesome/fontawesome-free";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
+import "@fortawesome/fontawesome-free/css/solid.css";
+
+import "bootstrap-icons/font/bootstrap-icons.css";
